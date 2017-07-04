b9b1fe8f7fdb44e8b8fe6d132a4c1bff41f2b369
Cezary Statkiewicz <cezio@thelirium.net>
2017-07-04 17:17:48 +0200

    init

--- deployer/__init__.py
+++ deployer/__init__.py
@@ -3,6 +3,7 @@
 
 import os
 import json
+from subprocess import Popen
 
 from flask import Flask, Response, request
 
@@ -10,25 +11,30 @@ app = Flask('deployer')
 
 
 
-@app.route('/incoming/')
+@app.route('/incoming/', methods=["POST"])
 def incoming():
     r = request
     config_path = os.environ.get('DEPLOYER_CONFIG')
     if not config_path:
-        return Response(response='no deployment config', status_code=500)
+        return Response(response='no deployment config', status=500)
     response = 'ok'
     code = 200
-    with open(confi_path, 'rt') as f:
+    with open(config_path, 'rt') as f:
         config = json.load(f)
 
         command = config['command']
-        env = config['env']
-
-        p = Popen(command, shell=True, env=env)
+        env = config['environment']
+        nenv = os.environment.copy()
+        nenv.update(env)
+        p = Popen(command, shell=True, env=nenv)
         ret = p.wait()
         if ret:
             code=500
             response='error during execution: {}'.format(ret)
 
-    r = Response(response=response, status_code=code)
-    return resp
+    r = Response(response=response, status=code)
+    return r
+
+
+if __name__ == '__main__':
+    app.run()
--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@
 import os
 from distutils.core import setup
 
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
 setup(
     name="deployer",
     version="0.1",
     author="Cezary Statkiewicz",
     author_email="c.statkiewicz@gmail.com",
     description="simple deployment server",
-    long_description=(read('README.rst')),
+    long_description=read('README.rst'),
     # Full list of classifiers can be found at:
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
